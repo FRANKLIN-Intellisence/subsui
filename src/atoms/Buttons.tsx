49188553b9128781014cea81849cbb9e3207913a
FRANKLIN-Intellisence <franklinsolaty@gmail.com>
2025-03-07 17:23:45 +0100

    started TicketVerification Page

--- src/atoms/Buttons.tsx
+++ src/atoms/Buttons.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 const Button = (props: ButtonProps) => {
   return (
     <a href="">
-      <button className="bg-[#00ffff] text-[#000] px-4 py-4 rounded-[5px] text-xl font-semibold cursor-pointer">
+      <button className="bg-[#00ffff] text-[#000] px-4 py-4 rounded-[5px] text-[1rem] md:text-[1.1rem] font-semibold cursor-pointer">
         {props.name} {props.link}
       </button>
     </a>
--- src/pages/TicketVerification.tsx
+++ src/pages/TicketVerification.tsx
@@ -1,17 +1,26 @@
+import Nav from '../atoms/Nav.tsx'
 import Buttons from '../atoms/Buttons.tsx'
 const TicketVerification = () => {
     return (
-        <div className='flex flex-col items-center justify-between bg-[#000000] pt-[5rem] pb-[5rem] min-h-full'>
+        <div>
             <div>
-                <h2 className='text-[3rem] text-[#00ffff]'>Ticket Verification</h2>
+                <Nav />
             </div>
-            <div className='bg-[#2963c6] text-[#fff] w-[35%] m-auto text-center py-[3rem] rounded-[10px]'>
-                <form action="" className='flex flex-col gap-[2rem] w-[80%] m-auto'>
-                    <input type="text" placeholder="Enter your full name" className='h-[3rem]'/>
-                    <input type="email" placeholder="Enter your email address" className='h-[3rem]'/>
-                    <input type="text" placeholder="Ticket number" className='h-[3rem]'/>
-                    <Buttons name='Verify Ticket' />
-                </form>
+            <div className='flex flex-col gap-[2rem] items-center justify-between bg-[#000022] pt-[1rem] pb-[3rem] md:pb-[7rem] min-h-full'>
+                <div>
+                    <h2 className='text-[1.8rem] md:text-[3rem] text-[#fff]'>Ticket Verification</h2>
+                </div>
+                <div className='bg-[#fff] text-[#000000] w-[95%] md:w-[50%] lg:w-[35%] m-auto text-center py-[3rem] rounded-[10px] shadow-[#00ffff] shadow-md'>
+                    <form action="" className='flex flex-col gap-[2rem] w-[90%] md:w-[80%] m-auto '>
+                        <input type="text" placeholder="Enter your full name" className='h-[3rem] text-[#000] font-semibold rounded-[5px]
+                                                                                        pl-2 outline-[#000022] outline-2'/>
+                        <input type="email" placeholder="Enter your email address" className='h-[3rem] text-[#000] font-semibold rounded-[5px]
+                                                                                         pl-2 outline-[#000022] outline-2'/>
+                        <input type="text" placeholder="Ticket number" className='h-[3rem] text-[#000000] font-semibold rounded-[5px]
+                                                                                 pl-2 outline-[#000022] outline-2'/>
+                        <Buttons name='Verify Ticket' />
+                    </form>
+                </div>
             </div>
         </div>
     )
