72271a64686ac3892a3bd9279698092e83876798
Winifred John <89465179+johnwinifred@users.noreply.github.com>
2025-03-07 14:15:52 +0100

    New landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,21 +1,24 @@
 import Buttons from '../atoms/Buttons.js'
-// import Landing from '../assets/landing.jpg'
+import Landing from '../assets/landing2.png'
 const LandingPage = () => {
     return (
-        <div className='bg-[#000022] text-[#ffffff] font-sans-serif'>
-            <div className='flex flex-row items-center justify-between py-8 px-20'>
-                <h1 className='text-[#00ffff] text-[3rem]'>SUI</h1>
+        <div className='bg-[#000022] text-[#ffffff] font-custom w-full flex flex-col'>
+            <div className='flex flex-row items-center justify-between py-8 px-[10rem]'>
+                <h1 className='text-[#00ffff] text-[3rem] font-custom'>SUI</h1>
                 <Buttons name="Connect Wallet"/>
             </div>
 
-            <div className='flex flex-row items-center justify-center h-[80vh] '>
-                <div className='font-sans-serif'>
-                <h2 className='text-[8rem] text-[#00ffff]' >Subsui</h2>
-                <h1 className='text-[5rem] text-gradient'>Your Ticketing Journey Starts Here</h1>
+            <div className='flex flex-row items-center justify-between h-[80vh]  px-[10rem] '>
+                <div className='font-sans-serif w-[60%]'>
+                <h2 className='text-[5rem] text-[#00ffff]'>Subsui</h2>
+                <div className='flex flex-col'>
+                <p className='text-[5rem] text-[#ffffff] mb-[-2rem]'>Your Ticketing Journey</p>
+                <p className='text-[5rem] text-gradient'> Starts Here.</p>
+                </div>
                 <p className='text-3xl mb-20'>create your tickets, log customer tickets and handle event subscription</p>
                 <Buttons name="Create Your First Ticket Here!"/>
                 </div>
-                {/* <img src= {Landing} alt="" /> */}
+                <img src= {Landing} alt="" className='w-[40rem]'/>
                 
             </div>
             
