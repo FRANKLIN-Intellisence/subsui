88a1d57303b9c37c50cb6087af835b6872251a77
Winifred John <89465179+johnwinifred@users.noreply.github.com>
2025-03-07 12:51:35 +0100

    added landing page

--- src/atoms/Buttons.jsx
+++ src/atoms/Buttons.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
     return (
         <a href="">
-            <button className="bg-[#00ffff] text-[#fff] px-4 py-2 rounded-[5px] font-semibold cursor-pointer">
+            <button className="bg-[#00ffff] text-[#000] px-4 py-4 rounded-[5px] text-xl font-semibold cursor-pointer">
                 {props.name}
             </button>
         </a>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,24 @@
 import Buttons from '../atoms/Buttons.jsx'
+// import Landing from '../assets/landing.jpg'
 const LandingPage = () => {
     return (
-        <div>
+        <div className='bg-[#000022] text-[#ffffff] font-sans-serif'>
             <div className='flex flex-row items-center justify-between py-8 px-20'>
-                <h1 className='text-[#00ffff] text-[2rem]'>SUI</h1>
+                <h1 className='text-[#00ffff] text-[3rem]'>SUI</h1>
                 <Buttons name="Sign Up"/>
             </div>
-            <div>
-                <h2></h2>
-                <img src="" alt="" />
+
+            <div className='flex flex-row items-center justify-center h-[80vh] '>
+                <div className='font-sans-serif'>
+                <h2 className='text-[8rem] text-[#00ffff]' >Subsui</h2>
+                <h1 className='text-[5rem] text-gradient'>Your Ticketing Journey Starts Here</h1>
+                <p className='text-3xl mb-20'>create your tickets, log customer tickets and handle event subscription</p>
+                <Buttons name="Create Your First Ticket Here!"/>
+                </div>
+                {/* <img src= {Landing} alt="" /> */}
+                
             </div>
-            <div></div>
+            
         </div>
     )
 }
