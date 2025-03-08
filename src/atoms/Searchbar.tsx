4b06ae19abada882ee2778ba26d4d10c333abba1
Winifred John <89465179+johnwinifred@users.noreply.github.com>
2025-03-08 11:51:05 +0100

    updated upcoming-events page

--- src/atoms/Searchbar.tsx
+++ src/atoms/Searchbar.tsx
@@ -7,13 +7,13 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ query, setQuery }) => {
     return (
-        <div className="flex justify-center my-4">
+        <div className="flex justify-center">
             <input
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 placeholder="Search events..."
-                className="p-2 rounded-md w-1/2"
+                className="px-8 py-2 rounded-3xl my-4 border-2 border-[#ffffff]  w-2/6"
             />
         </div>
     );
--- src/pages/Upcoming-events.tsx
+++ src/pages/Upcoming-events.tsx
@@ -52,7 +52,8 @@ const UpcomingEvents: React.FC = () => {
         <Nav />
         <div className='font-custom bg-[#000022] text-[#ffffff] w-full min-h-screen'>
             <h1 className='text-4xl text-center my-8'>Upcoming Events</h1>
-            <SearchBar query={query} setQuery={setQuery} />
+          
+            <SearchBar query={query} setQuery={setQuery}  />
             <ul className='grid gap-4 px-8 w-full'>
                 {filteredEvents.map(event => (
                     <li key={event.id} className='flex flex-row items-center bg-[#010131] p-4 rounded-md w-full justify-between'>
@@ -63,15 +64,15 @@ const UpcomingEvents: React.FC = () => {
                         </div>
                         <div className='text-[#ffffff] mt-4 w-[20%]'>
                         <span className='flex items-center gap-2'>
-                                <FaCalendarAlt className='text-[#0000ff]' />
+                                <FaCalendarAlt className='text-[#00ffff]' />
                                 <p>{new Date(event.time).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
                             </span>
                             <span className='flex items-center gap-2 mt-2'>
-                                <IoTimeOutline className='text-[#0000ff]' />
+                                <IoTimeOutline className='text-[#00ffff]' />
                                 <p>{new Date(event.time).toLocaleTimeString()}</p>
                             </span>
                             <span className='flex items-center gap-2 mt-2'>
-                                <FaLocationDot className='text-[#0000ff]' />
+                                <FaLocationDot className='text-[#00ffff]' />
                                 <p>{event.location}</p>
                             </span>
                             
